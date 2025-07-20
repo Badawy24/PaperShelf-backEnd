ecc766c3d9983229e285f7251bf06b555307c81f
Abdelrahman Badawy <88353210+Badawy24@users.noreply.github.com>
2025-07-20 19:36:22 +0300

    Update categoryController.js

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,10 +2,10 @@
 
 // List your categories and their icon image paths
 const categoryList = [
-    { name: 'Classic', image: '/uploads/category-icons/classic.png' },
-    { name: 'History', image: '/uploads/category-icons/history.png' },
-    { name: 'Programming', image: '/uploads/category-icons/programming.png' },
-    { name: 'Romance', image: '/uploads/category-icons/romance.png' },
+    { name: 'Classic', image: 'uploads/category-icons/classic.png' },
+    { name: 'History', image: 'uploads/category-icons/history.png' },
+    { name: 'Programming', image: 'uploads/category-icons/programming.png' },
+    { name: 'Romance', image: 'uploads/category-icons/romance.png' },
 ];
 
 const getCategoriesWithIcons = (req, res) => {
